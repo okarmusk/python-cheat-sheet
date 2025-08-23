5f62f107bb27fde835b0fa7426c9e58698d1c7aa
Konrad Okarmus <okarmusk@outlook.com>
2025-08-23 23:14:45 +0200

    More about classes

--- src/python_cheat_sheet/08_classes/classes_02.py
+++ src/python_cheat_sheet/08_classes/classes_02.py
@@ -6,5 +6,9 @@ class Second:
     def __init__(self, l: list):
         self.lst = l
 
+    def method_a(self, arg: str):
+        logging.info(arg)
+
 second = Second([1, 2, 3, 4, 5])
 logging.info(second.lst)
+second.method_a('Display a method_a call')
---
+++ src/python_cheat_sheet/08_classes/classes_03.py
@@ -0,0 +1,27 @@
+import logging
+
+logging.basicConfig(level = logging.INFO)
+
+class Third:
+    variable = 'Third class shared variable' # CLass shared variable, till override via instance
+
+    def __init__(self):
+        pass
+
+    # class method setter
+    @classmethod
+    def set_variable(cls, value):
+        cls.variable = value
+
+t0 = Third()
+logging.info(t0.variable)
+t1 = Third()
+t1.variable = 'Changed Third class shared variable'
+logging.info(t0.variable) # 'Third class shared variable'
+logging.info(t1.variable) # Display 'Changed Third class shared variable'
+# To change it for whole class access via class
+Third.variable = 'Changed Third class shared variable'
+logging.info(t0.variable) # New variable value visible
+
+Third.set_variable('Changed again Third class shared variable')
+logging.info(t0.variable) # New variable value visible
